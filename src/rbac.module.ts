de93cee7b8adbb4ef2da6767553fb004b5b67ae0
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2023-05-01 14:13:26 +0100

    updated rbac forDynamic Typings to Include Constructor Params

--- src/rbac.module.ts
+++ src/rbac.module.ts
@@ -8,104 +8,105 @@ import { ICacheRBAC } from './interfaces/cache.rbac.interface';
 
 @Global()
 @Module({
-    providers: [
-        RbacService,
-        StorageRbacService,
-        Reflector,
-    ],
-    imports: [],
-    exports: [
-        RbacService,
-    ],
+  providers: [RbacService, StorageRbacService, Reflector],
+  imports: [],
+  exports: [RbacService],
 })
 export class RBAcModule {
-    private static cache?: any | ICacheRBAC;
-    private static cacheOptions?: { KEY?: string, TTL?: number };
+  private static cache?: any | ICacheRBAC;
+  private static cacheOptions?: { KEY?: string; TTL?: number };
 
-    static useCache(
-        cache: any | ICacheRBAC,
-        options?: {
-            KEY?: string,
-            TTL?: number
+  static useCache(
+    cache: any | ICacheRBAC,
+    options?: {
+      KEY?: string;
+      TTL?: number;
+    },
+  ) {
+    RBAcModule.cache = cache;
+    RBAcModule.cacheOptions = options;
+    return RBAcModule;
+  }
+
+  static forRoot(
+    rbac: IStorageRbac,
+    providers?: any[],
+    imports?: any[],
+  ): DynamicModule {
+    return RBAcModule.forDynamic(
+      /* tslint:disable */
+      class {
+        async getRbac(): Promise<IStorageRbac> {
+          return rbac;
+        }
+      },
+      providers,
+      imports,
+    );
+  }
+
+  static forDynamic<T extends new (...args: any[]) => IDynamicStorageRbac>(
+    rbac: T,
+    providers?: any[],
+    imports?: any[],
+  ): DynamicModule {
+    const inject = [ModuleRef, rbac];
+    const commonProviders = [];
+    if (RBAcModule.cache) {
+      commonProviders.push(RBAcModule.cache, {
+        provide: 'ICacheRBAC',
+        useFactory: (cache: ICacheRBAC): ICacheRBAC => {
+          return RBAcModule.setCacheOptions(cache);
         },
-    ) {
-        RBAcModule.cache = cache;
-        RBAcModule.cacheOptions = options;
-        return RBAcModule;
+        inject: [RBAcModule.cache],
+      });
+      inject.push(RBAcModule.cache);
     }
 
-    static forRoot(
-        rbac: IStorageRbac,
-        providers?: any[],
-        imports?: any[],
-    ): DynamicModule {
+    commonProviders.push(
+      ...[
+        ...(providers || []),
+        rbac,
+        {
+          provide: StorageRbacService,
+          useFactory: async (
+            moduleRef: ModuleRef,
+            rbacService: IDynamicStorageRbac,
+            cache?: ICacheRBAC,
+          ) => {
+            return new StorageRbacService(
+              moduleRef,
+              rbacService,
+              RBAcModule.setCacheOptions(cache),
+            );
+          },
+          inject,
+        },
+      ],
+    );
 
-        return RBAcModule.forDynamic(
-            /* tslint:disable */
-            class {
-                async getRbac(): Promise<IStorageRbac> {
-                    return rbac;
-                };
-            },
-            providers,
-            imports,
-        );
+    return {
+      module: RBAcModule,
+      providers: commonProviders,
+      imports: [...(imports || [])],
+    };
+  }
+
+  private static setCacheOptions(cache?: ICacheRBAC) {
+    if (!cache || RBAcModule.cacheOptions) {
+      return cache;
+    }
+    if (!RBAcModule.cacheOptions) {
+      return cache;
+    }
+    if (RBAcModule.cacheOptions.KEY) {
+      cache.KEY = RBAcModule.cacheOptions.KEY;
     }
 
-    static forDynamic(
-        rbac: new () => IDynamicStorageRbac,
-        providers?: any[],
-        imports?: any[],
-    ): DynamicModule {
-        const inject = [ModuleRef, rbac];
-        const commonProviders = [];
-        if (RBAcModule.cache) {
-            commonProviders.push(RBAcModule.cache, {
-                provide: 'ICacheRBAC',
-                useFactory: (cache: ICacheRBAC): ICacheRBAC => {
-                    return RBAcModule.setCacheOptions(cache);
-                },
-                inject: [RBAcModule.cache],
-            });
-            inject.push(RBAcModule.cache);
-        }
-
-        commonProviders.push(...[
-            ...(providers || []),
-            rbac,
-            {
-                provide: StorageRbacService,
-                useFactory: async (moduleRef: ModuleRef, rbacService: IDynamicStorageRbac, cache?: ICacheRBAC) => {
-                    return new StorageRbacService(moduleRef, rbacService, RBAcModule.setCacheOptions(cache));
-                },
-                inject,
-            },
-        ]);
-
-        return {
-            module: RBAcModule,
-            providers: commonProviders,
-            imports: [
-                ...(imports || []),
-            ],
-        };
+    if (RBAcModule.cacheOptions.TTL) {
+      cache.TTL = RBAcModule.cacheOptions.TTL;
     }
 
-    private static setCacheOptions(cache?: ICacheRBAC) {
-        if (!cache || RBAcModule.cacheOptions) {
-            return cache;
-        }
-        if (!RBAcModule.cacheOptions) {
-            return cache;
-        }
-        if (RBAcModule.cacheOptions.KEY) {
-            cache.KEY = RBAcModule.cacheOptions.KEY;
-        }
-
-        if (RBAcModule.cacheOptions.TTL) {
-            cache.TTL = RBAcModule.cacheOptions.TTL;
-        }
-
-        return cache;
-    }
+    return cache;
+  }
 }
